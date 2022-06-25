46a51702162c1718e5adc16438422cdfbbb15848
ethanly <ethanly@students.cs.ubc.ca>
2022-06-25 05:26:59 -0700

    added styling changes and edited mui

--- my-app/src/components/CreateAccount.js
+++ my-app/src/components/CreateAccount.js
@@ -1,10 +1,11 @@
 import styles from "./CreateAccount.module.scss"
+import { Button } from '@material-ui/core';
 
 function CreateAccount() {
     return (
         <div className={styles.createBox}>
             <form className={styles.formControl}>
-                <b>Create an Account</b>
+                <label>Create an Account</label>
                 
                 <input placeholder="First name" size="20"/>
                 <input placeholder="Surname" size="20"/>
@@ -14,7 +15,7 @@ function CreateAccount() {
                 <input placeholder="Password" size="20"/>
                 <input placeholder="Re-enter Password" size="20"/>
                 <div className={styles.controls}>
-                    <button>Create</button>    
+                    <Button>Create</Button>    
                 </div>
             </form>
         </div>
