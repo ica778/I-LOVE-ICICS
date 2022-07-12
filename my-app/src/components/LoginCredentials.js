fbecf9065d4841e58b09e33a6999c5780867c0f7
ica778 <71621160+ica778@users.noreply.github.com>
2022-07-12 02:52:11 -0700

    login userId persists in state

--- my-app/src/components/LoginCredentials.js
+++ my-app/src/components/LoginCredentials.js
@@ -1,21 +1,32 @@
 import { updateUserId } from '../actions/index';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Button } from '@material-ui/core';
-import styles from './LoginCredentials.module.scss'
+import styles from './LoginCredentials.module.scss';
+import { useState } from 'react';
 
 function LoginCredentials() {
     const dispatch = useDispatch();
 
+    const [usernameInput, setUsernameInput] = useState("");
+    function setUsername(val) {
+        setUsernameInput(val.target.value);
+    }
+
+    const [passwordInput, setPasswordInput] = useState("");
+    function setPassword(val) {
+        setPasswordInput(val.target.value);
+    }
+
     const handleLogin = () => {
-        dispatch(updateUserId(1234));
+        dispatch(updateUserId(usernameInput));
     }
 
     return (
         <div className={styles.loginBox}>
             <form className={styles.formControl}>
                 <label>Login</label>
-                <input placeholder="Username" size="20"/>
-                <input placeholder="Password" size="20"/>
+                <input value={usernameInput} onChange={setUsername} placeholder="Username" size="20"/>
+                <input value={passwordInput} onChange={setPassword} placeholder="Password" size="20"/>
             </form>
             <Button className={styles.input} onClick={handleLogin}>Login</Button>
         </div>
